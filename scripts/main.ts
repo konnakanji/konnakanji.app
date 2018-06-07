d835c818c1c79f10c6592bae8e2f49e4a37d6290
Eduard Urbach <e.urbach@gmail.com>
2018-06-08 08:33:39 +0900

    Added error message

--- scripts/main.ts
+++ scripts/main.ts
@@ -1,6 +1,11 @@
 import AppView from "elements/app-view/app-view"
 import KanjiView from "elements/kanji-view/kanji-view"
 
+// Error message if browser is too old
+if(!("customElements" in window)) {
+	alert("Your browser is too old and doesn't support web components!")
+}
+
 // Custom element names must have a dash in their name
 const elements = new Map<string, Function>([
 	["app-view", AppView],
